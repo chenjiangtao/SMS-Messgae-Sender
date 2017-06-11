75d4d43b104a00e0ae292e85969f8b537fa5392d
frankdevhub <frankdevhub@163.com>
2017-06-12 00:05:09 +0800

    SMSMessage sender through http
    SMSMessage sender through http

--- sms/src/main/java/com/fangcs/sms/protocol/Reader.java
+++
@@ -1,7 +0,0 @@
-package com.fangcs.sms.protocol;
-
-import java.io.IOException;
-
-public interface Reader {
-    Message read() throws IOException;
-}
--- sms/src/main/java/com/fangcs/sms/protocol/Writer.java
+++
@@ -1,8 +0,0 @@
-package com.fangcs.sms.protocol;
-
-import java.io.IOException;
-
-
-public interface Writer {
-    void write(Message message) throws IOException;
-}
